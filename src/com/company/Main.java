149d02c2cd8140b529df1c6b786a0407dc07dc42
Junia George <junegal11@gmail.com>
2017-09-24 18:23:41 -0700

    Created csv's of all the mappings

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,30 +1,124 @@
 package com.company;
 
+import javafx.util.Pair;
+
+import java.io.*;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
 public class Main {
 
     //class variables
-    public HashMap<String, String> verbatim = new HashMap<String, String>();
-    public HashMap<String, String> fractionSymbols = new HashMap<String, String>();
-    public HashMap<String, String> measureAbbr = new HashMap<String, String>();
-    public HashMap<String, String> money = new HashMap<String, String>();
-    public HashMap<String, String> time = new HashMap<String, String>();
+    public static HashMap<String, String> verbatim = new HashMap<String, String>();
+    public static HashMap<String, String> fractionSymbols = new HashMap<String, String>();
+    public static HashMap<String, String> measureAbbr = new HashMap<String, String>();
+    public static HashMap<String, String> money = new HashMap<String, String>();
+    public static HashMap<String, String> time = new HashMap<String, String>();
 
     public static void main(String[] args) {
-        System.out.println("sorry i fucked up the git stuff and had to make a new one teds");
+        String csvFile = "C:/Users/juneg/IdeaProjects/KaggleEnglishTextNormalization/Data/en_train.csv/en_train.csv";
+        try{
+            readTrainCsv(csvFile);
+        }
+        catch (Exception ex) {
+            System.out.println(ex);
+        }
     }
 
-    public void readAndCreateHashmaps(String type, String input, String output){
+    public static void readTrainCsv(String csvFile) throws Exception {
+        BufferedReader br = null;
+        String line = "";
+        String csvSplitBy = ",";
+        LinkedList<Pair<String, String>> l = new LinkedList<Pair<String, String>>();
+        HashMap<String, Pair<String, String>> h = new HashMap<String, Pair<String, String>>();
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+                // use comma as separator
+                String[] lines = line.split(csvSplitBy);
+                if(lines.length > 5){
+                    String four = lines[lines.length-1];
+                    int ind = lines.length - 1;
+                    if(!(four.length() > 1 && four.startsWith("\"") && four.endsWith("\""))) {
+                        ind--;
+                        while (!lines[ind].startsWith("\"")) {
+                            four = lines[ind] + "," + four;
+                            ind--;
+                        }
+                        four = lines[ind] + "," + four;
+                    }
+
+                    String three = lines[3];
+                    for(int tind = 4; tind < ind; tind++){
+                        three = three + "," + lines[tind];
+                    }
+
+                    lines[3] = three;
+                    lines[4] = four;
+                }
+
+                try{
+                    readAndCreateHashmaps(lines[2].substring(1, lines[2].length()-1),
+                            lines[3].substring(1, lines[3].length()-1),
+                            lines[4].substring(1, lines[4].length()-1));
+                }
+                catch(Exception ex){
+                    System.out.println(ex);
+                    System.out.println(lines[2]);
+                    System.out.println(lines[3]);
+                    System.out.println(lines[4]);
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        writeHashMapToCsv("Verbatim.csv", verbatim);
+        writeHashMapToCsv("Fraction.csv", fractionSymbols);
+        writeHashMapToCsv("Measure.csv", measureAbbr);
+        writeHashMapToCsv("Money.csv", money);
+        writeHashMapToCsv("Time.csv", time);
+    }
+
+    public static void writeHashMapToCsv(String file, HashMap<String, String> map) throws Exception {
+        String eol = System.getProperty("line.separator");
+        try (Writer writer = new FileWriter(file)) {
+            for (HashMap.Entry<String, String> entry : map.entrySet()) {
+                writer.append(entry.getKey())
+                        .append(',')
+                        .append(entry.getValue())
+                        .append(eol);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace(System.err);
+        }
+    }
+
+    public static void readAndCreateHashmaps(String type, String input, String output){
+        System.out.println(type);
         if(type.equals("VERBATIM")){
-            verbatim.put(input, output);
+            verbatim.put(input.trim(), output.trim());
             return;
         }
 
         if(type.equals("FRACTION")){
             String[] splitOutput = output.split("and");
-            fractionSymbols.put(input.substring(input.length() - 1),
-                    splitOutput[splitOutput.length-1]);
+            fractionSymbols.put(input.substring(input.length() - 1).trim(),
+                    splitOutput[splitOutput.length-1].trim());
+            return;
         }
 
         if(type.equals("MEASURE")){
@@ -49,27 +143,49 @@ public class Main {
                 i++;
             }
 
-            //we need to be able to separate the after words to isolate the translation for the abbr
-
+            measureAbbr.put(sym.trim(), output.trim());
+            return;
         }
 
         if(type.equals("MONEY")){
-
+            String[] splitSym = input.split("[0-9]|[,]|[.]");
+            for(String s: splitSym) {
+                if(!s.equals("")){
+                    money.put(s.trim(), output.trim());
+                    return;
+                }
+            }
         }
 
         if(type.equals("TIME")){
-
+            String[] splitSym = input.split("[0-9]|[:]");
+            for(String s: splitSym) {
+                if(!s.equals("")){
+                    time.put(s.trim(), output.trim());
+                    return;
+                }
+            }
         }
     }
 
-    public String identifyAndNormalize(String before){
+    public static String identifyAndNormalize(String before){
         before = before.trim();
         if(before.matches("\\p{Punct}*")){
             return before;
         }
 
+        //check if verbatim
+
+
         if(before.matches("[a-z]*") || before.matches("[A-Z][a-z]*")){
-            return before;
+            if(before.contains("a") || before.contains("e") || before.contains("i")
+                    || before.contains("o") || before.contains("u") || before.contains("y")){
+                return before;
+            }
+            else{
+                return spreadAcronym(before);
+            }
+
         }
 
         if(before.matches("[A-Z]*")){
@@ -142,7 +258,7 @@ public class Main {
         return null;
     }
 
-    private String readYear(String before) {
+    public static String readYear(String before) {
         int num = Integer.parseInt(before);
         if(num >= 2000 && num < 2010){
             return readNumber(before);
@@ -152,7 +268,7 @@ public class Main {
         }
     }
 
-    private String readRomanNumeral(String before) {
+    public static String readRomanNumeral(String before) {
         int decimal = 0;
 
         String romanNumeral = before.toUpperCase();
@@ -218,7 +334,7 @@ public class Main {
         return readNumber(Integer.toString(decimal));
     }
 
-    public String spreadAcronym(String input){
+    public static String spreadAcronym(String input){
         String output = "";
         input = input.toLowerCase();
         for(char c: input.toCharArray()){
@@ -227,13 +343,13 @@ public class Main {
         return output.trim();
     }
 
-    public String readNumber(String input){
+    public static String readNumber(String input){
         int number = Integer.parseInt(input);
 
         return "";
     }
 
-    public String readDecimal(String input){
+    public static String readDecimal(String input){
         String[] split = input.split(".");
         String output = readNumber(split[0]);
 
